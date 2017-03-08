951b8f656c3770f3536122fc3645e0d324eeb214
Kenneth Edzel <kennethedzelazarcon@live.com>
2017-03-09 06:41:24 +0800

    Initial Commit

---
+++ src/main/java/com/pluralsight/service/GoalServiceImpl.java
@@ -0,0 +1,15 @@
+package com.pluralsight.service;
+
+import org.springframework.stereotype.Service;
+
+import com.pluralsight.model.Goal;
+
+@Service("goalService")
+public class GoalServiceImpl implements GoalService {
+
+	public Goal save(Goal goal) {
+		
+		return null;
+	}
+
+}
